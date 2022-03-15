c1c09beb20ff77af3d186be1ce159f1480e7de77
Constantin Ziesche <constantin.ziesche@bosch.com>
2022-03-15 12:13:49 +0100

    Updated LICENSE

--- HelloAssetAdministrationShell/HelloAssetAdministrationShellService.cs
+++ HelloAssetAdministrationShell/HelloAssetAdministrationShellService.cs
@@ -1,12 +1,12 @@
 /*******************************************************************************
-* Copyright (c) 2020, 2021 Robert Bosch GmbH
+* Copyright (c) 2022 Bosch Rexroth AG
 * Author: Constantin Ziesche (constantin.ziesche@bosch.com)
 *
 * This program and the accompanying materials are made available under the
-* terms of the Eclipse Distribution License 1.0 which is available at
-* https://www.eclipse.org/org/documents/edl-v10.html
+* terms of the MIT License which is available at
+* https://github.com/eclipse-basyx/basyx-dotnet/blob/main/LICENSE
 *
-* 
+* SPDX-License-Identifier: MIT
 *******************************************************************************/
 using BaSyx.API.ServiceProvider;
 using BaSyx.Models.AdminShell;
